caddd6a3fdbad2e2c9f0eebd2e3eec39850000da
const-ae <artjom31415@googlemail.com>
2020-01-22 17:02:49 +0100

    Implement estimate_size_factors()

--- R/estimate_size_factors.R
+++ R/estimate_size_factors.R
@@ -9,5 +9,19 @@
 #' Not exported
 #' @keywords internal
 estimate_size_factors <- function(Y){
-
+  stopifnot(is.matrix(Y))
+  logGeoMeans <- rowMeans(log(Y + 0.5))
+  logGeoMeans_mat <- matrix(logGeoMeans, nrow = length(logGeoMeans), ncol=ncol(Y))
+  Y2 <- Y
+  Y2[Y2 == 0] <- NA
+  sf <- exp(matrixStats::colMedians(log(Y2) - logGeoMeans_mat, na.rm=TRUE))
+  all_zero_column <- is.nan(sf)
+  if(any(all_zero_column)){
+    sf <- sf/exp(mean(log(sf), na.rm=TRUE))
+    sf[all_zero_column] <- 0.001
+  }else{
+    # stabilize size factors to have geometric mean of 1
+    sf <- sf/exp(mean(log(sf)))
+  }
+  sf
 }
