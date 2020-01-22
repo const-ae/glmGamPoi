e72188569efc88449e8bae64372011bcecd8626f
const-ae <artjom31415@googlemail.com>
2020-01-22 17:04:34 +0100

    Make sure that estimate_betas and estimate_betas_one_group return same list

--- R/estimate_betas.R
+++ R/estimate_betas.R
@@ -33,7 +33,7 @@ estimate_betas <- function(Y, model_matrix, offset_matrix,
   betaRes <- fitBeta(Y, model_matrix, exp(offset_matrix), dispersions, beta_mat_init,
           tolSEXP = 1e-8, maxitSEXP =  100, minmuSEXP = 1e-6)
 
-  betaRes
+  list(Beta = betaRes$beta_mat, iterations = betaRes$iter)
 }
 
 
@@ -42,20 +42,16 @@ estimate_betas <- function(Y, model_matrix, offset_matrix,
 #'
 #'
 #' @keywords internal
-estimate_betas_one_group <- function(Y, offset_matrix,
-                                     dispersions = NULL, beta_vec_init = NULL){
-
+estimate_betas_one_group <- function(Y, offset_matrix,  dispersion, beta_vec_init = NULL){
   if(is.null(beta_vec_init)){
     beta_vec_init <- log(rowMeans(Y / exp(offset_matrix)))
   }
-  if(is.null(dispersions)){
-    mu_mat <- exp(beta_mat_init %*% t(model_matrix) + offset_matrix)
-    dispersions <- estimate_overdispersions(Y, mu_mat, do_cox_reid_adjustment = TRUE)
-  }
 
-
-  betaRes <- fitBeta_one_group(Y, offset_matrix, thetas = dispersions,
+  betaRes <- fitBeta_one_group(Y, offset_matrix, thetas = dispersion,
                                beta_start_values = beta_vec_init,
                                tolerance = 1e-8, maxIter = 100)
-  betaRes
+
+
+  list(Beta = matrix(betaRes$beta, nrow = nrow(Y), ncol = 1),
+       iterations = betaRes$iter)
 }
