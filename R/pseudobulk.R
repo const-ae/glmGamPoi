62cbe0a96f099170f522fbc779d83e9b4e7304cd
const-ae <artjom31415@googlemail.com>
2023-01-03 16:39:30 +0100

    Change 'pseudobulk_sce' to 'pseudobulk'
    * Fix bug related to missing assay names
    * Support 'n()' inside summary arguments
    * Support summarizing a plain matrix

--- R/pseudobulk.R
+++ R/pseudobulk.R
@@ -1,7 +1,7 @@
 
 #' Create a 'SingleCellExperiment' containing pseudo-bulk samples
 #'
-#' @param sce a 'SingleCellExperiment' or an object of a related class
+#' @param data a 'SingleCellExperiment' or an object of a related class
 #' @param group_by an unquoted expression that can refer to columns in
 #'   the 'colData()'. All observations with the same factor level are aggregated.
 #'   The argument follows the same logic as `dplyr::group_by()`. The argument must
@@ -10,7 +10,7 @@
 #'   must produce a value of length 1. The arguments follow the same logic
 #'   as `dplyr::summarize()`.
 #' @param aggregation_functions a named list with functions that are used to
-#'   aggregate the assays in the `sce`.
+#'   aggregate the assays in the `data`.
 #'
 #' @return a SingleCellExperiment object
 #'
@@ -21,13 +21,23 @@
 #'                     size = rnorm(n = 50, mean = 40, sd = 15))
 #'  Y <- matrix(rnbinom(n = 100 * 50, mu = 3, size = 1/3.1), nrow = 100, ncol = 50)
 #'  sce <- SingleCellExperiment(Y, colData = data)
-#'  aggr_sce <- pseudobulk_sce(sce, group_by = vars(sample, celltype), size = mean(size))
+#'  aggr_sce <- pseudobulk(sce, group_by = vars(sample, celltype), size = mean(size))
 #'  aggr_sce
 #'  colData(aggr_sce)
 #'
 #' @export
-pseudobulk_sce <- function(sce, group_by, ...,
-                           aggregation_functions = list(counts = "rowSums2", .default = "rowMeans2")){
+pseudobulk <- function(data, group_by, ...,
+                       aggregation_functions = list(counts = "rowSums2", .default = "rowMeans2"),
+                       col_data = NULL){
+
+  col_data <- get_col_data(data, col_data)
+  if(is.matrix(data)){
+    if(any((data %% 1) > 1e-10)){
+      warning("'data' is treated as a count matrix even though it contains non-integer values.")
+    }
+    data <- SummarizedExperiment::SummarizedExperiment(list(counts = data), colData = col_data)
+  }
+
   if(missing(group_by)) stop("The 'group_by' must not be missing.")
   tryCatch({
     if(inherits(group_by, "uneval") || !rlang::is_quosures(group_by)) stop("The 'group_by' argument must be wrapped using 'vars()'")
@@ -35,19 +45,18 @@ pseudobulk_sce <- function(sce, group_by, ...,
     stop(e, "\n", "The 'group_by' argument must be wrapped using 'vars()'")
   })
 
-  index_seq <- seq_len(ncol(sce))
-  col_data <- as.data.frame(SummarizedExperiment::colData(sce))
+  index_seq <- seq_len(ncol(data))
   if(! ".default" %in% names(aggregation_functions)){
     aggregation_functions$.default <- "rowMeans"
   }
 
   # Make vector that is used to
   groups <- lapply(group_by, rlang::eval_tidy, data = col_data)
-  if(! all(lengths(groups) == 1 | lengths(groups) == ncol(sce))){
+  if(! all(lengths(groups) == 1 | lengths(groups) == ncol(data))){
     stop("The argument 'group_by' has lengths ", paste0(lengths(groups), collapse = ","), ", which does not match the number of columns ",
-         "in 'sce' (", ncol(sce), ")")
+         "in 'data' (", ncol(data), ")")
   }else if(any(lengths(groups) == 1)){
-    groups[lengths(groups) == 1] <- lapply(groups[lengths(groups) == 1], \(x) rep_len(x, ncol(sce)))
+    groups[lengths(groups) == 1] <- lapply(groups[lengths(groups) == 1], \(x) rep_len(x, ncol(data)))
   }
 
   if(is.null(groups)){
@@ -57,38 +66,54 @@ pseudobulk_sce <- function(sce, group_by, ...,
   }
 
   # Aggregate all assays
-  new_assays <- lapply(SummarizedExperiment::assayNames(sce), function(assay_name){
+  assay_names <- SummarizedExperiment::assayNames(data)
+  if(is.null(assay_names)){
+    assay_names <- seq_len(length(SummarizedExperiment::assays(data)))
+  }
+  new_assays <- lapply(assay_names, function(assay_name){
     aggr_fnc <- get_aggregation_function(assay_name, aggregation_functions)
-    data_mat <- SummarizedExperiment::assay(sce, assay_name)
+    data_mat <- SummarizedExperiment::assay(data, assay_name)
     new_data_mat <- do.call(cbind, lapply(group_split, function(idx){
       aggr_fnc(data_mat[,idx,drop=FALSE])
     }))
-    rownames(new_data_mat) <- rownames(sce)
+    rownames(new_data_mat) <- rownames(data)
     new_data_mat
   })
-  names(new_assays) <- SummarizedExperiment::assayNames(sce)
+  if(! is.null(SummarizedExperiment::assayNames(data))){
+    names(new_assays) <- assay_names
+  }
 
   # Aggregate reduced dims
-  new_red_dims <- lapply(SingleCellExperiment::reducedDimNames(sce), function(red_name){
-    aggr_fnc <- get_aggregation_function(red_name, aggregation_functions)
-    tdata_mat <- SingleCellExperiment::reducedDim(sce, red_name)
-    if(is(tdata_mat, "LinearEmbeddingMatrix")){
-      data_mat <- t(SingleCellExperiment::sampleFactors(tdata_mat))
-      new_data_mat <- do.call(cbind, lapply(group_split, function(idx){
-        aggr_fnc(data_mat[,idx,drop=FALSE])
-      }))
-      SingleCellExperiment::LinearEmbeddingMatrix(t(new_data_mat), SingleCellExperiment::featureLoadings(tdata_mat),
-                                                  factorData = SingleCellExperiment::factorData(tdata_mat))
-    }else{
-      data_mat <- t(tdata_mat)
-      new_data_mat <- do.call(cbind, lapply(group_split, function(idx){
-        aggr_fnc(data_mat[,idx,drop=FALSE])
-      }))
-      rownames(new_data_mat) <- rownames(data_mat)
-      t(new_data_mat)
+  if(is(data, "SingleCellExperiment")){
+    red_assay_names <- SingleCellExperiment::reducedDimNames(data)
+    if(is.null(red_assay_names)){
+      red_assay_names <- seq_len(length(SingleCellExperiment::reducedDims(data)))
     }
-  })
-  names(new_red_dims) <- SingleCellExperiment::reducedDimNames(sce)
+    new_red_dims <- lapply(red_assay_names, function(red_name){
+      aggr_fnc <- get_aggregation_function(red_name, aggregation_functions)
+      tdata_mat <- SingleCellExperiment::reducedDim(data, red_name)
+      if(is(tdata_mat, "LinearEmbeddingMatrix")){
+        data_mat <- t(SingleCellExperiment::sampleFactors(tdata_mat))
+        new_data_mat <- do.call(cbind, lapply(group_split, function(idx){
+          aggr_fnc(data_mat[,idx,drop=FALSE])
+        }))
+        SingleCellExperiment::LinearEmbeddingMatrix(t(new_data_mat), SingleCellExperiment::featureLoadings(tdata_mat),
+                                                    factorData = SingleCellExperiment::factorData(tdata_mat))
+      }else{
+        data_mat <- t(tdata_mat)
+        new_data_mat <- do.call(cbind, lapply(group_split, function(idx){
+          aggr_fnc(data_mat[,idx,drop=FALSE])
+        }))
+        rownames(new_data_mat) <- rownames(data_mat)
+        t(new_data_mat)
+      }
+    })
+    if(! is.null(SingleCellExperiment::reducedDimNames(data))){
+      names(new_red_dims) <- red_assay_names
+    }
+  }else{
+    new_red_dims <- NULL
+  }
 
   # Aggregate column data
   dots_cap <- rlang::enquos(...)
@@ -96,10 +121,15 @@ pseudobulk_sce <- function(sce, group_by, ...,
     dot <- dots_cap[[dot_idx]]
     dot_name <- names(dots_cap)[dot_idx]
     if(is.null(dot_name) || dot_name == ""){
-      rlang::as_label(dot)
+      dot_name <- rlang::as_label(dot)
     }
+
     unname(do.call(c, lapply(group_split, function(idx){
-      res <- rlang::eval_tidy(dot, col_data[idx,,drop=FALSE])
+      functions <- rlang::new_environment(list(n = function(){length(idx)}))
+      mask <- rlang::new_data_mask(bottom = rlang::new_environment(col_data[idx,,drop=FALSE], parent = functions), top = functions)
+      mask$.fns <- rlang::as_data_pronoun(functions)
+      mask$.data <- rlang::as_data_pronoun(mask)
+      res <- rlang::eval_tidy(dot, data = mask)
       if(length(res) != 1) stop("Illegal result in aggregation of '", dot_name,
                                 "'. The aggregated value has to be of length 1. However, it has length ", length(res))
       res
--- tests/testthat/test-pseudobulk.R
+++ tests/testthat/test-pseudobulk.R
@@ -7,18 +7,18 @@ test_that("forming pseudobulk works", {
   colnames(Y) <- paste0("cell_", seq_len(50))
   sce <- SingleCellExperiment::SingleCellExperiment(list(counts = Y, logcounts = log(Y + 1)),
                                                     colData  = data)
-  expect_error(pseudobulk_sce(sce))
-  expect_error(pseudobulk_sce(sce, NULL))
-  expect_error(pseudobulk_sce(sce, vars(1:2)))
-  expect_error(pseudobulk_sce(sce, group_by = vars(city), age = diff(age)))
+  expect_error(pseudobulk(sce))
+  expect_error(pseudobulk(sce, NULL))
+  expect_error(pseudobulk(sce, vars(1:2)))
+  expect_error(pseudobulk(sce, group_by = vars(city), age = diff(age)))
 
-  pseudobulk_sce(sce, group_by = vars(city), age = mean(age), head(fav_food, n = 1))
+  pseudobulk(sce, group_by = vars(city), age = mean(age), head(fav_food, n = 1))
 
-  tmp <- pseudobulk_sce(sce, group_by = vars(city, fav_food))
+  tmp <- pseudobulk(sce, group_by = vars(city, fav_food))
   cd <- SummarizedExperiment::colData(tmp)
   expect_equal(rownames(cd), as.character(paste0(cd$city, ".", cd$fav_food)))
 
-  tmp2 <- pseudobulk_sce(sce[,1], group_by = vars(city), age = mean(age), fav_food = head(fav_food, n = 1))
+  tmp2 <- pseudobulk(sce[,1], group_by = vars(city), age = mean(age), fav_food = head(fav_food, n = 1))
   colnames(tmp2) <- "cell_1"
   SummarizedExperiment::colData(tmp2)$city <- as.character(SummarizedExperiment::colData(tmp2)$city)
   expect_equal(SummarizedExperiment::colData(sce[,1])[,c("city", "age", "fav_food")],
@@ -27,11 +27,11 @@ test_that("forming pseudobulk works", {
   SummarizedExperiment::colData(sce)$fact <- factor(sample(letters[1:3], 50, replace = TRUE),
                                                     levels = letters[1:4])
 
-  tmp3 <- pseudobulk_sce(sce, group_by = vars(fact), age = mean(age))
+  tmp3 <- pseudobulk(sce, group_by = vars(fact), age = mean(age))
   expect_equal(levels(SummarizedExperiment::colData(tmp3)$fact), letters[1:4])
   expect_equal(tmp3$age[1], mean(sce$age[sce$fact == tmp3$fact[1]]))
 
-  tmp4 <- pseudobulk_sce(sce, group_by = vars(fact), aggregation_functions = list(counts = matrixStats::rowMins))
+  tmp4 <- pseudobulk(sce, group_by = vars(fact), aggregation_functions = list(counts = matrixStats::rowMins))
   expect_equal(unname(SummarizedExperiment::assay(tmp4, "counts")[,"a"]),
                matrixStats::rowMins(SummarizedExperiment::assay(sce[,sce$fact == "a"], "counts")))
   expect_equal(unname(SummarizedExperiment::assay(tmp4, "logcounts")[,"b"]),
@@ -40,20 +40,22 @@ test_that("forming pseudobulk works", {
   pca <- stats::prcomp(t(SummarizedExperiment::assay(sce,"logcounts")), rank. = 2)
   SingleCellExperiment::reducedDim(sce, "PCA") <- pca$x
   SingleCellExperiment::reducedDim(sce, "PCA2") <- SingleCellExperiment::LinearEmbeddingMatrix(pca$x, pca$rotation)
-  tmp5 <- pseudobulk_sce(sce, group_by = vars(fav_food))
+  tmp5 <- pseudobulk(sce, group_by = vars(fav_food))
   expect_equal(dim(SingleCellExperiment::reducedDim(tmp5, "PCA")), c(3, 2))
   expect_equal(dim(SingleCellExperiment::reducedDim(tmp5, "PCA2")), c(3, 2))
 
   # Try advanced metaprogramming features
   fav_food <- "test"
-  pseudobulk_sce(sce, group_by = vars(city), age = mean(.data$age), .env$fav_food)
+  pseudobulk(sce, group_by = vars(city), age = mean(.data$age), .env$fav_food)
 
   f <- function(arg, arg2){
-    pseudobulk_sce(sce, group_by = vars({{arg}}), mean({{arg2}}))
+    pseudobulk(sce, group_by = vars({{arg}}), mean({{arg2}}))
   }
   f(city, arg2 = age)
 
-  pseudobulk_sce(sce, group_by = vars(city), aggregation_functions = list(.default = Matrix::rowSums))
+  pseudobulk(sce, group_by = vars(city), aggregation_functions = list(.default = Matrix::rowSums))
+  # Try context specific functions
+  pseudobulk(sce, group_by = vars(city), n = n())
 })
 
 
