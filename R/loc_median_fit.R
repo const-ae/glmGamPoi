772b661f4a046d7b436632fbfd33f924283a412c
Nathan Lubock <nate@octant.bio>
2023-11-13 16:19:51 -0800

    feat: add subsample to loc_median_fit

--- R/loc_median_fit.R
+++ R/loc_median_fit.R
@@ -13,6 +13,8 @@
 #'   `npoints = length(x) * fraction`.
 #' @param weighted a boolean that indicates if a weighted median is calculated.
 #' @param ignore_zeros should the zeros be excluded from the fit
+#' @param sample_fraction fraction of the data to estimate local median on
+#' @param seed control the random sampling of `sample_fraction`
 #'
 #' @details
 #' This function is low-level implementation detail and should usually not be
@@ -27,39 +29,64 @@
 #'
 #'   plot(x, y)
 #'   fit <- loc_median_fit(x, y, fraction = 0.1)
+#'   fit2 <- loc_median_fit(x, y, fraction = 0.1, sample_fraction = 0.75)
 #'   points(x, fit, col = "red")
+#'   points(x, fit, col = "blue")
 #'
 #'
 #' @export
 loc_median_fit <- function(x, y, fraction = 0.1, npoints = max(1, round(length(x) * fraction)),
-                           weighted = TRUE, ignore_zeros = FALSE){
-  # Make sure npoints is valid
-  npoints <- max(1, npoints)
-  npoints <- min(length(x), npoints)
-
-  stopifnot(length(x) == length(y))
-  if(length(x) == 0){
+                           weighted = TRUE, ignore_zeros = FALSE, sample_fraction = 1, seed=42){
+  if (length(x) != length(y)) {
+    stop("x and y must be of the same length.")
+  }
+  if (length(x) == 0) {
     return(numeric(0L))
   }
-  stopifnot(npoints > 0 && npoints <= length(x))
-  ordered_y <- y[order(x)]
-  half_points <- floor(npoints/2)
-  start <- half_points + 1
-  end <- length(x) - half_points
-  weights <- dnorm(seq(-3, 3, length.out = half_points * 2 + 1))
-
-  if(end < start){
-    if(weighted){
-      wm <- matrixStats::weightedMedian(y, w =  dnorm(seq(-3, 3, length.out = length(x))))
-      return(rep(wm, length(x)))
-    }else{
-      return(rep(median(y), length(x)))
-    }
+  if (!is.numeric(sample_fraction) || sample_fraction <= 0 || sample_fraction > 1) {
+    stop("sample_fraction must be a numeric value between 0 and 1.")
+  }
+  if (is.null(npoints)) {
+    npoints <- max(1, round(length(x) * fraction))
+  }
+  if (npoints <= 0 || npoints > length(x)) {
+    stop("npoints must be a positive integer less than or equal to the length of x.")
   }
 
-  res <- rep(NA, length(x))
-  idx <- start
+  if(sample_fraction != 1){
+    set.seed(seed)
+    subset_size <- round(sample_fraction * length(x))
+    sample_indices <- sample(1:length(x), size = subset_size)
+    x_sample <- x[sample_indices]
+    y_sample <- y[sample_indices]
+  } else {
+    x_sample <- x
+    y_sample <- y
+  }
+  
+  # Make sure npoints is valid
+  npoints <- max(1, npoints)
+  npoints <- min(length(x_sample), npoints)
 
+  ordered_indices <- order(x_sample)
+  ordered_y <- y_sample[ordered_indices]
+  half_points <- floor(npoints / 2)
+  start <- half_points + 1
+  end <- length(x_sample) - half_points
+  weights <- dnorm(seq(-3, 3, length.out = half_points * 2 + 1))
+  
+  if(end < start){
+    if(weighted){
+      wm <- matrixStats::weightedMedian(y_sample, w =  dnorm(seq(-3, 3, length.out = length(x_sample))))
+      return(rep(wm, length(x)))
+    }else{
+      return(rep(median(y_sample), length(x)))
+    }
+  }
+  
+  res <- rep(NA, length(x_sample))
+  idx <- start
+  
   while(idx <= end){
     selection <- ordered_y[seq(idx - half_points, idx + half_points)]
     if(ignore_zeros){
@@ -79,12 +106,18 @@ loc_median_fit <- function(x, y, fraction = 0.1, npoints = max(1, round(length(x
     }
     idx <- idx + 1
   }
-
-  # Fill up NA's at the beginning and the end
-  res[seq(1, start - 1)] <- res[start]
-  res[seq(length(x), end+1)] <- res[end]
-
-  res[order(order(x))]
+  
+  # Handle edge cases by extending the closest computed median
+  res[seq_len(start - 1)] <- res[start]
+  res[seq(end + 1, length(x_sample))] <- res[end]
+  
+  if (sample_fraction == 1) {
+    # Reorder the results to match the original x
+    res_ordered <- res
+    res_ordered[ordered_indices] <- res
+    return(res_ordered)
+  } else {
+    interp_func <- approxfun(x_sample[ordered_indices], res, method = "linear", yleft = res[start], yright = res[end])
+    return(interp_func(x))
+  }
 }
-
-
